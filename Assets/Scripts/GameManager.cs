182f1db6d6151c36fe8b9f11d526c5270dfef626
luciano90a <luciano_90lsv@hotmail.com>
2022-11-30 21:54:05 -0300

    sss
    sss

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -42,6 +43,6 @@ public class GameManager : MonoBehaviour
     }
     public void closeapp()
     {
-        Application.Quit();
+        SceneManager.LoadScene("MenuInicial");
     }
 }
